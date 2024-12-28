6d51c726baebcb47910044142ed2cdd585c3787f
Filipe Marques <Filipe.Nascimento.Marques1@gmail.com>
2024-12-28 15:40:23 +0000

    Code With Smote

--- smote.py
+++ smote.py
@@ -3,34 +3,22 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
 
-table_X, table_y, features, target_name, df = load_data('PetFinder_dataset.csv')
-
-#Smote for adopted or not
-def smoteadopted(table_X,table_y):
+def smoteadopted(table_X,table_y,features):
     
-
-    X_train, X_test, y_train, y_test = train_test_split(table_X,table_y), test_size=0.2, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(table_X,table_y, test_size=0.2, random_state=0)
 
     smote = SMOTE(sampling_strategy='auto',random_state=0)
     X_smote, y_smote = smote.fit_resample(X_train, y_train)
     df_resampled = pd.DataFrame(X_smote, columns=features)
     df_resampled['Adopted'] = y_smote
-
-    print(pd.Series(y_train).value_counts())
-    print(pd.Series(y_smote).value_counts())
     return X_smote, y_smote, df_resampled
 
 #Smote for adoptionspeed
-def smoteadoptionspeed(table_X,table_y):
+def smoteadoptionspeed(table_X,table_y,features):
     X_train, X_test, y_train, y_test = train_test_split(table_X, table_y, test_size=0.2, random_state=0)
     smote = SMOTE(sampling_strategy='auto',random_state=0)
     X_smote, y_smote = smote.fit_resample(X_train, y_train)
     df_resampled = pd.DataFrame(X_smote, columns=features)
     df_resampled['AdoptionSpeed'] = y_smote
-
-    print(pd.Series(y_train).value_counts())
-    print(pd.Series(y_smote).value_counts())
     return X_smote, y_smote, df_resampled
 
-#smoteadopted()
-smoteadoptionspeed()
